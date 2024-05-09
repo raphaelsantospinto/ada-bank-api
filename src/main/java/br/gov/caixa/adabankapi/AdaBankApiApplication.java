9bbb80367910bb5427080460c9538f97d6037eed
Raphael dos Santos Pinto <raphaelsantospinto@gmail.com>
2024-05-09 00:08:43 -0300

    Initial commit

---
+++ src/main/java/br/gov/caixa/adabankapi/AdaBankApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.caixa.adabankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaBankApiApplication.class, args);
+	}
+
+}
