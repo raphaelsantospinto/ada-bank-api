d72e6226e30fab457c47426b82f93c3cd440c8a1
Raphael dos Santos Pinto <raphaelsantospinto@gmail.com>
2024-05-09 00:19:58 -0300

    Commit inicial

---
+++ src/main/java/br/gov/caixa/adabankapi/controller/ClientController.java
@@ -0,0 +1,16 @@
+package br.gov.caixa.adabankapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+
+    @GetMapping
+    public String getClient(){
+        return "Hello World!";
+    }
+
+}
